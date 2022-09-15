87f1fb24b6e3ac1e520129aa8d038057e8eff53e
Aditya Raj <adityarajofficial0096@gmail.com>
2022-09-15 09:22:47 +0530

    Created JavaScript for Responsive Navigation

---
+++ index.js
@@ -0,0 +1,15 @@
+/*---------------------------------------->
+<--        Responsive Navigation        -->
+-----------------------------------------*/
+
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+    // alert("hi");
+    nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
