9e43e8c47c303e64264f0449a6ec2693512aa46f
Aditya Raj <adityarajofficial0096@gmail.com>
2022-09-15 14:32:15 +0530

    Added Media Queries in JavaScript

--- index.js
+++ index.js
@@ -41,3 +41,43 @@ const observer = new IntersectionObserver(
 observer.observe(sectionHero);
 
 
+
+/*--------------------------------------------------------->
+<--          Adding Media Queries in JavaScript          -->
+----------------------------------------------------------*/
+
+
+function myFunction(widthSize) {
+    if (widthSize.matches) {
+        // If media query matches
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    } else {
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+}
+
+const widthSize = window.matchMedia("(max-width: 780px)");
+
