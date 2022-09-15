903b1cc5a4b241e164070c27e98544d59e9cad86
Aditya Raj <adityarajofficial0096@gmail.com>
2022-09-15 17:23:13 +0530

    Created JavaScript for Animated Counter Numbers

--- index.js
+++ index.js
@@ -189,3 +189,50 @@ const imgObserver = new IntersectionObserver(lazyImg, {
 });
 
 imgObserver.observe(imgRef);
+
+
+/*------------------------------------------------------------>
+<--                 Aminated Counter Number                 -->
+-------------------------------------------------------------*/
+
+
+const workSection = document.querySelector(".section-work-data");
+
+const workSectionObserve = (entries) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    console.log(entries);
+
+
+    const counterNum = document.querySelectorAll(".counter-numbers");
+    // Console.log(counterNum);
+    const speed = 200;
+
+    counterNum.forEach((curNumber) => {
+        const updateNumber = () => {
+            const targetNumber = parseInt(curNumber.dataset.number);
+            // Console.log(targetNumber);
+            const initialNumber = parseInt(curNumber.innerText);
+            // Console.log(initialNumber);
+            const incrementNumber = Math.trunc(targetNumber / speed);
+            // I am Adding the Value to the Main Number
+            // console.log(incrementNumber);
+
+            if (initialNumber < targetNumber) {
+                curNumber.innerText = `${initialNumber + incrementNumber}+`;
+                setTimeout(updateNumber, 10);
+            } else {
+                curNumber.innerText = `${targetNumber}+`;
+            }
+
+        };
+        updateNumber();
+    });
+};
+
+const workSecObserver = new IntersectionObserver(workSectionObserve, {
+    root: null,
+    threshold: 0,
+});
+
+workSecObserver.observe(workSection);
